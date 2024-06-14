3368ff944026c39fb353ef1e8856f101a2fd0ac7
Viknesh Venkatesan <viknesh2022@gmail.com>
2024-06-14 20:04:26 +0530

    friday final

---
+++ Assets/Scripts/Scratch Card/scratchCardEffect.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scratchCardEffect : MonoBehaviour
+{
+    public GameObject maskPrefab;
+    private bool isPressed = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        var mousePos = Input.mousePosition;
+        mousePos.z = 10;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        if(isPressed == true)
+        {
+            GameObject maskSprite = Instantiate(maskPrefab.gameObject, mousePos, Quaternion.identity);
+            maskSprite.transform.parent = gameObject.transform;
+        }
+
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Invoke("reveal", 5);
+            isPressed = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isPressed = false;
+        }
+    }
+
+    void reveal()
+    {
+        Destroy(this.gameObject);
+    }
+}
