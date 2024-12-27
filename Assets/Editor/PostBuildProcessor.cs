0558166238b9cba84c026d4dd11ab59cd94c06fd
Viknesh Venkatesan <viknesh2022@gmail.com>
2024-12-27 10:17:09 +0530

    IOS support is added

---
+++ Assets/Editor/PostBuildProcessor.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public class PostBuildProcessor
+{
+    [PostProcessBuild] 
+    public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
+    {
+        // Check if the build target is iOS
+        if (buildTarget == BuildTarget.iOS)
+        {
+            
+            string plistPath = Path.Combine(path, "Info.plist");
+
+            PlistDocument plist = new PlistDocument();
+            plist.ReadFromFile(plistPath);
+
+            plist.root.SetString(
+                "NSMicrophoneUsageDescription",
+                "This app requires microphone access to record audio for Speech Therpy"
+            );
+            
+            plist.WriteToFile(plistPath);
+        }
+    }
+}
