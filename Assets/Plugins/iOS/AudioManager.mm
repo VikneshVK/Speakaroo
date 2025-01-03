4c38736e3e6e06677e54aef55a2e8623223de216
Viknesh Venkatesan <viknesh2022@gmail.com>
2025-01-03 14:39:51 +0530

    plugin update

--- Assets/Plugins/iOS/AudioManager.mm
+++ Assets/Plugins/iOS/AudioManager.mm
@@ -4,17 +4,19 @@ extern "C" {
     void SetAudioToLoudSpeaker() {
         NSError *error = nil;
         AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-        
-        // Set the audio session category to Playback
-        [audioSession setCategory:AVAudioSessionCategoryPlayback error:&error];
-        
+
+        // Set the audio session category to PlayAndRecord
+        [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord
+                      withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker
+                            error:&error];
+
         // Activate the audio session
         [audioSession setActive:YES error:&error];
-        
+
         if (error) {
             NSLog(@"Error setting AVAudioSession: %@", error.localizedDescription);
         } else {
-            NSLog(@"Audio session set to loudspeaker successfully.");
+            NSLog(@"Audio session set to PlayAndRecord successfully.");
         }
     }
 }
